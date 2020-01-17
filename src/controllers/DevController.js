f8597c47e5d7a5ab404bc77218b5dc60d92ee7b8
Pedro Kretikouski Roque Junior <pedro.junior@ringa.com.br>
2020-01-17 11:55:41 -0300

    Remove responsabilidade de desmembrar string

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
 
 module.exports = {
 
@@ -16,9 +17,9 @@ module.exports = {
         if(!dev) {
 
             const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
-        
             const { name = login, avatar_url, bio} = apiResponse.data
-            const techsArray = techs.split(',').map(tech => tech.trim())
+            const techsArray = parseStringAsArray(techs)
+            
             const location = {
                 type: 'Point',
                 coordinates: [longitude, latitude]
